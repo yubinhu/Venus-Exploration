f0e9497836cd7688b22acdd378c377bdd1753ee8
Harvey <you@example.com>
2022-11-20 23:45:29 -0800

    running correct ps with n=500

--- bayesopt_param_search.sh
+++ bayesopt_param_search.sh
@@ -1,3 +1,3 @@
-nohup python Scripts/BayesOptPS.py --run_num '1' --n 300
-nohup python Scripts/BayesOptPS.py --run_num '2' --n 300
-nohup python Scripts/BayesOptPS.py --run_num '3' --n 300
+python Scripts/BayesOptPS.py --run_num '1' --n 500
+python Scripts/BayesOptPS.py --run_num '2' --n 500
+python Scripts/BayesOptPS.py --run_num '3' --n 500
